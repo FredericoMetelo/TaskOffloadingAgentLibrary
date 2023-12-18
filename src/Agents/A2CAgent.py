9c9090021722835a6afb3dd16ec891418f92cd81
FredericoMetelo <fc.metelo@campus.fct.unl.pt>
2023-12-18 16:21:21 +0000

    Merging with changes that I forgot to pull... The A2C Network is done, finishing the learning loop on the agent.

---
+++ src/Agents/A2CAgent.py
@@ -0,0 +1,124 @@
+import numpy as np
+from peersim_gym.envs.PeersimEnv import PeersimEnv
+
+from src.Agents.Agent import Agent
+from src.Agents.Networks.A2C import ActorCritic
+from src.Utils import utils
+
+
+class A2CAgent(Agent):
+    """
+    Actor-Critic Agent
+    Sources:
+    - https://www.youtube.com/watch?v=OcIx_TBu90Q&t=1050s  | Video with tips of how to implement A3C
+    """
+
+    def __init__(self, input_shape, action_space, output_shape, agents, learning_rate=0.7, gamma=0.4,
+                 control_type="A2C"):
+        super().__init__(input_shape, action_space, output_shape, learning_rate)
+        self.gamma = gamma
+        self.control_type = control_type
+
+        self.A2C = ActorCritic(lr=learning_rate, input_dims=self.input_shape, fc1_dims=256, fc2_dims=256,
+                               n_actions=self.actions)
+
+        self.agent_states = {
+            agent: {
+                'state': [],
+                'action': [],
+                'reward': [],
+                'next_state': [],
+                'done': []
+            } for agent in agents
+        }
+
+    def train_loop(self, env: PeersimEnv, num_episodes, print_instead=False, controllers=None):
+        super().train_loop(env, num_episodes, print_instead, controllers)
+        # See page 14 from: https://arxiv.org/pdf/1602.01783v2.pdf
+        scores, episodes, avg_scores, obj, avg_episode = [], [], [], [], []
+        steps_per_return = 5
+        for i in range(num_episodes):
+            # Prepare variables for the next run
+            dones = [False for _ in controllers]
+            agent_list = env.agents
+            total_reward = 0
+            step = 0
+            total_steps = 0
+            # Episode metrics
+            score = 0.0
+
+            # Reset the state
+            states, _ = env.reset()
+            states = utils.flatten_state_list(states, agent_list)
+
+            while not utils.is_done(dones):
+                print(f'Step: {step}\n')
+                # Interaction Step:
+                targets = {agent: np.floor(self.__get_action(np.array([states[idx]]))) for idx, agent in
+                           enumerate(agent_list)}
+                actions = utils.make_action(targets, agent_list)
+
+                next_states, rewards, dones, _, _ = env.step(actions)
+                next_states = utils.flatten_state_list(next_states, agent_list)
+                total_reward_in_step = self.__store_agent_step_data(states, actions, rewards, next_states, dones,
+                                                                    agent_list)
+                score += total_reward_in_step
+                # Advance to next iter
+                states = next_states
+                step += 1
+                total_steps += 1
+                # Update metrics
+
+                self.__learn(s=self.state_memory, a=self.action_memory, r=self.reward_memory,
+                             s_next=self.new_state_memory, k=step, fin=self.terminal_memory)
+
+                if step % steps_per_return == 0 or dones:
+                    # src for the update code https://github.com/dxyang/DQN_pytorch/blob/master/learn.py
+                    # Update target network
+                    self.target_Q_value.load_state_dict(self.Q_value.state_dict())
+                    step = 0
+            # Update final metrics
+            avg_episode.append(score / total_steps)
+            scores.append(score)
+            episodes.append(i)
+            avg_score = np.mean(scores[-100:])
+            avg_scores.append(avg_score)
+
+        self.__plot(episodes, scores=scores, avg_scores=avg_scores, per_episode=avg_episode,
+                    print_instead=print_instead)
+        self.__plot2(episodes, title=self.control_type, per_episode=avg_episode, print_instead=print_instead)
+
+    def __learn(self, s, a, r, s_next, k, fin):
+        pass
+
+    def get_action(self, observation):
+        return self.A2C.choose_action(observation)
+
+
+    def __store_agent_step_data(self, states, actions, rewards, next_states, dones, agent_list):
+        total_rwrd = 0
+        for idx, agent in enumerate(agent_list):
+            # Update history
+            agent_data = self.agent_states[agent]
+            agent_data['state'].add(states[idx])
+            agent_data['action'].add(actions[agent])
+            agent_data['reward'].add(rewards[agent])
+            agent_data['next_state'].add(next_states[idx])
+            agent_data['done'].add(dones[idx])
+            total_rwrd += rewards[agent]
+        return total_rwrd
+
+    def __get_agent_step_data(self, agent):
+        agent_data = self.agent_states[agent]
+        return agent_data['state'], agent_data['action'], agent_data['reward'], agent_data['next_state'], agent_data[
+            'done']
+
+    def __clean_agent_step_data(self, agents):
+        for agent in agents:
+            self.agent_states[agent] = {
+                'state': [],
+                'action': [],
+                'reward': [],
+                'next_state': [],
+                'done': []
+            }
---
+++ src/Agents/Networks/A2C.py
@@ -0,0 +1,131 @@
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Categorical
+import os
+
+
+class ActorCritic(nn.Module):
+    """
+    Actor Critic Network:
+    Based on three resources:
+    - https://github.com/simoninithomas/simple-A2C/blob/master/3_A2C-nstep-TUTORIAL.ipynb
+    - https://www.youtube.com/watch?v=OcIx_TBu90Q&t=1050s  | Video with tips of how to implement A3C
+    - https://arxiv.org/pdf/1602.01783.pdf | Original A3C Paper
+    """
+
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions, gamma=0.99):
+        super(ActorCritic, self).__init__()
+
+        self.gamma = gamma
+        self.n_actions = n_actions
+
+        self.state_memory = []
+        self.reward_memory = []
+        self.action_memory = []
+        self.done_memory = []
+
+        # Shared Network, both Critic and actoir have the same stump.
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        # Network
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        # Actor Head:
+        self.actor = nn.Linear(self.fc2_dims, self.n_actions)  # Approx Policy = Outputs an Action.
+        # Critic Head:
+        self.critic = nn.Linear(self.fc2_dims, 1)  # Approx Vp = Outputs a Value Function.
+
+        # Adam is a variation of SGD
+        self.optimizer = T.optim.Adam(self.parameters(), lr=lr)
+
+        # MSE is the loss function
+        # self.loss = nn.MSELoss()
+        # GPU support, in torch we need to specify where we are sending the Network.
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def remember(self, state, action, reward, done):
+        self.state_memory.append(state)
+        self.reward_memory.append(reward)
+        self.action_memory.append(action)
+        self.done_memory.append(done)
+
+    def clear_memory(self):
+        self.state_memory = []
+        self.reward_memory = []
+        self.action_memory = []
+        self.done_memory = []
+
+    def forward(self, state):
+        # This is the forward pass of the network, it is called when we call the network with an input
+        # It is the same as the forward pass of a normal NN. In torch we have to define the forward pass
+        # but because we inherit from nn.Module, we get the backpropagation for free.
+        layer1 = T.relu(self.fc1(state))
+        layer2 = T.relu(self.fc2(layer1))
+        # Actor Head:
+        actor = F.relu(self.actor(layer2))
+        # Critic Head:
+        critic = F.relu(self.critic(layer2))
+
+        return actor, critic  # Logits, Value
+
+        # Note to self: Phil in the tutorial does not immediately compute the softmax, he does it later on.
+        # But he also never uses the output of actor directly.
+
+    def calculate_returns(self, done):
+        # Calculate the returns of the episode
+        states = T.tensor(self.state_memory, dtype=T.float)  # We must convert the state vector to a tensor
+        _, v = self.forward(states)
+        # Define last state as done, using this expression allows having t-step returns instead of episode returns.
+        R = v[-1] * (1 - int(done))
+
+        # This computes the reward/return for each time-step in the episode. This must be done from the end to the start
+        # of the episode, because the return at time-step t depends on the return at time-step t+1. We then reverse the
+        # list so that the first element is the return at time-step 0.                  
+        batch_return = []
+        for reward in self.reward_memory[::-1]:
+            R = reward + self.gamma * R
+            batch_return.append(R)
+        batch_return.reverse()
+        batch_return = T.tensor(batch_return, dtype=T.float)
+        return batch_return
+
+    def calculate_loss(self, done):
+        # Loss: delta_teta' log(pia | s_t, teta_actor)) * A(s_t, a_t, teta_actor, teta_critic)
+        # Where:
+        # A(s_t, a_t; teta_actor, teta_critic) = (R_t+k * V(s_t+k; teta_critic) - V(s_t; teta_critic))
+        states = T.tensor(self.states, dtype=T.float)
+        actions = T.tensor(self.actions, dtype=T.float)
+
+        returns = self.calculate_returns(done)
+
+        pi, values = self.forward(states)  # Note: This instantiates the networks.
+        values = values.squeeze()  # This removes all size one dimensions on the states tensor. Which includes the
+        critic_loss = (
+                              returns - values) ** 2  # MSQ Error between the returns at each time step and the value function at that time step.
+
+        probs = T.softmax(pi, dim=1)  # Softmax converts the output of the actor into a probability distribution.
+        dist = Categorical(probs)  # This creates a discrete distribution from the probabilities.
+        log_probs = dist.log_prob(actions)
+        actor_loss = -log_probs * (returns - values)
+
+        total_loss = (actor_loss + critic_loss).mean()
+
+    def choose_action(self, observation):
+        # This is the forward pass of the network, it is called when we call the network with an input
+        # It is the same as the forward pass of a normal NN. In torch we have to define the forward pass
+        # but because we inherit from nn.Module, we get the backpropagation for free.
+        state = T.tensor([observation], dtype=T.float).to(self.device)
+        actor, critic = self.forward(state)
+        probs = T.softmax(actor, dim=1)
+        dist = Categorical(probs)
+        action = dist.sample().numpy()[0]
+        return action
+
+    def remember_batch(self, states, actions, rewards, dones):
+        self.state_memory.append(states)
+        self.action_memory.append(actions)
+        self.reward_memory.append(rewards)
+        self.done_memory.append(dones)
